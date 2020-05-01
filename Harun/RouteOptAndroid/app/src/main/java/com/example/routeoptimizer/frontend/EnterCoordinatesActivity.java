f0dfcfee5260bbadc8b42cc58a13b7136122fc3a
Harun Can Surav <62554574+cansrv@users.noreply.github.com>
2020-05-01 21:56:18 +0300

    Few more GUI pages
    Added map locations interface ( may be deleted)

--- Harun/RouteOptAndroid/app/src/main/java/com/example/routeoptimizer/frontend/EnterCoordinatesActivity.java
+++ Harun/RouteOptAndroid/app/src/main/java/com/example/routeoptimizer/frontend/EnterCoordinatesActivity.java
@@ -2,11 +2,9 @@ package com.example.routeoptimizer.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.routeoptimizer.R;
 
