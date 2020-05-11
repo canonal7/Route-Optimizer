b86ced3186c3f62b901e6476cf9ae7ef62798eea
Harun Can Surav <62554574+cansrv@users.noreply.github.com>
2020-05-11 18:27:16 +0300

    Changed app icon

--- Harun/RouteOptAndroid/app/src/main/java/com/example/routeoptimizer/frontend/MainActivity.java
+++ Harun/RouteOptAndroid/app/src/main/java/com/example/routeoptimizer/frontend/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.routeoptimizer.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
 import com.example.routeoptimizer.R;
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -23,25 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String PREFS_NAME = "MyPrefsFile";
 
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 
-        try {
-            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Log.d("Comments", "First time");
             try {
-                fos.write(("").getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
+                fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
                 try {
-                    fos.close();
+                    fos.write(("").getBytes());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).commit();
         }
     }
 
